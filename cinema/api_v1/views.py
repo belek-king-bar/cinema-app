9eff09d3193838e2f51f4c9e37d48b98ccb7b82f
belek <belektolubaev@gmail.com>
2019-03-03 23:46:36 +0600

    Создание api viewSets

--- cinema/api_v1/views.py
+++ cinema/api_v1/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from webapp.models import Movie, Category, Hall, Seat, Show
+from rest_framework import viewsets
+from api_v1.serializers import MovieSerializer, HallSerializer, CategorySerializer, SeatSerializer, ShowSerializer
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all().order_by('-release_date')
+    serializer_class = MovieSerializer
+
+class HallViewSet(viewsets.ModelViewSet):
+    queryset = Hall.objects.all()
+    serializer_class = HallSerializer
+
+class ShowViewSet(viewsets.ModelViewSet):
+    queryset = Show.objects.all().order_by('start_datetime')
+    serializer_class = ShowSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
