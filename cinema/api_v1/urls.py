1f6708d19c2baf233232d9e23902cd3e0b6361a4
belek <belektolubaev@gmail.com>
2019-03-03 23:47:48 +0600

    Создание urls

---
+++ cinema/api_v1/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework import routers
+from api_v1 import views
+
+router = routers.DefaultRouter()
+router.register(r'movies', views.MovieViewSet)
+router.register(r'seat', views.SeatViewSet)
+router.register(r'show', views.ShowViewSet)
+router.register(r'category', views.CategoryViewSet)
+router.register(r'hall', views.HallViewSet)
+
+
+app_name = 'api_v1'
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
