a2fbd83ba0b4dae928bc83d1443eb01226d281a2
belek <belektolubaev@gmail.com>
2019-03-03 23:48:27 +0600

    Добавление моделей в admin.py

--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Movie, Category, Show, Hall, Seat
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'release_date']
+    ordering = ['-release_date']
+    search_fields = ['name', 'id']
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Category)
+admin.site.register(Show)
+admin.site.register(Seat)
+admin.site.register(Hall)
