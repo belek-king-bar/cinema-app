3346fde7224699916ac95623b5fd47d632056c38
belek <belektolubaev@gmail.com>
2019-03-06 23:57:56 +0600

    Создание serializaers для Билета, Брони и Скидки

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -1,29 +1,89 @@
-from webapp.models import Movie, Hall, Show, Seat, Category
+from webapp.models import Movie, Hall, Show, Seat, Category, Sale, Ticket, Booking
 from rest_framework import serializers
 
+class CategorySerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:category-detail')
+
+    class Meta:
+        model = Category
+        fields = ('url', 'id', 'name', 'description')
+
+
+class InlineCategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
+class InlineSeatSerializer(serializers.ModelSerializer):
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', source='hall', read_only=True)
+
+    class Meta:
+        model = Seat
+        fields = ('id', 'hall', 'hall_url', 'row', 'place')
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
+    category = InlineCategorySerializer(many=True, read_only=True)
+
     class Meta:
         model = Movie
-        fields = ('id', 'name', 'description', 'poster', 'release_date', 'finish_date', 'category')
+        fields = ('url', 'id', 'name', 'description', 'poster', 'release_date', 'finish_date', 'category')
 
 
 class HallSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:hall-detail')
+
     class Meta:
         model = Hall
-        fields = ('id', 'name')
+        fields = ('url', 'id', 'name')
+
 
 class SeatSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:seat-detail')
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', source='hall', read_only=True)
+
     class Meta:
         model = Seat
-        fields = ('id', 'hall', 'row', 'place')
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'description')
+        fields = ('url', 'id', 'hall', 'row', 'place')
 
 
 class ShowSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:show-detail')
+    movie_url = serializers.HyperlinkedRelatedField(view_name='api_v1:movie-detail', source='movie', read_only=True)
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', source='hall', read_only=True)
+
     class Meta:
         model = Show
-        fields = ('id', 'movie', 'hall', 'start_datetime', 'finish_datetime', 'price')
+        fields = ('url', 'id', 'movie', 'hall', 'start_datetime', 'finish_datetime', 'price', 'movie_url', 'hall_url')
+
+
+class SaleSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:sale-detail')
+
+    class Meta:
+        model = Sale
+        fields = ('url', 'id', 'name', 'how', 'start_date', 'finish_date')
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:ticket-detail')
+    show_url = serializers.HyperlinkedRelatedField(view_name='api_v1:show-detail', source='show', read_only=True)
+    seat_url = serializers.HyperlinkedRelatedField(view_name='api_v1:seat-detail', source='seat', read_only=True)
+    sale_url = serializers.HyperlinkedRelatedField(view_name='api_v1:sale-detail', source='sale', read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = ('url', 'id', 'show', 'show_url', 'seat', 'seat_url', 'sale', 'sale_url', 'go_back')
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:booking-detail')
+    show_url = serializers.HyperlinkedRelatedField(view_name='api_v1:show-detail', source='show', read_only=True)
+    seat = InlineSeatSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Booking
+        fields = ('url', 'id', 'code', 'show', 'show_url', 'seat', 'status', 'created_at', 'updated_at')
