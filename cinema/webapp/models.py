4375b9879f5780bf359633059f92324bfb7e138e
belek <belektolubaev@gmail.com>
2019-03-06 23:58:16 +0600

    Создание моделей для Билета, Брони и Скидки

--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import random
+import string
+from django.conf import settings
 
 # Create your models here.
 class Movie(models.Model):
@@ -7,7 +10,7 @@ class Movie(models.Model):
     poster = models.ImageField(upload_to='posters', null=True, blank=True)
     release_date = models.DateField()
     finish_date = models.DateField(null=True, blank=True)
-    category = models.ManyToManyField('Category', related_name="category", blank=True, verbose_name="Жанр")
+    category = models.ManyToManyField('Category', related_name="movie", blank=True, verbose_name="Жанр")
 
 
     def __str__(self):
@@ -31,21 +34,68 @@ class Hall(models.Model):
 
 
 class Seat(models.Model):
-    hall = models.ForeignKey(Hall, related_name="sitting_hall", verbose_name="Название зала", on_delete=models.PROTECT)
+    hall = models.ForeignKey(Hall, related_name="seat", verbose_name="Название зала", on_delete=models.PROTECT)
     row = models.CharField(max_length=15, verbose_name="Номер ряда")
     place = models.CharField(max_length=15, verbose_name="Номер места")
 
     def __str__(self):
-        return "%s - %s -> %s" % (self.hall, self.row, self.place)
+        return "%s - %s ряд  %s место" % (self.hall, self.row, self.place)
 
 class Show(models.Model):
-    movie = models.ForeignKey(Movie, related_name="movie", verbose_name="Название фильма", on_delete=models.PROTECT)
-    hall = models.ForeignKey(Hall, related_name="hall", verbose_name="Название зала", on_delete=models.PROTECT)
+    movie = models.ForeignKey(Movie, related_name="show", verbose_name="Название фильма", on_delete=models.PROTECT)
+    hall = models.ForeignKey(Hall, related_name="show", verbose_name="Название зала", on_delete=models.PROTECT)
     start_datetime = models.DateTimeField()
     finish_datetime = models.DateTimeField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return "%s - %s, %s - %s" % (self.movie, self.hall, self.start_datetime, self.price)
+        return "%s - %s, Сеанс в (%s) - %s сом" % (self.movie, self.hall, self.start_datetime.strftime('%d.%m.%Y %H:%M'), self.price)
+
+class Sale(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название скидки")
+    how = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(null=True, blank=True)
+    finish_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return "%s - %s" % (self.name, self.how)
 
 
+class Ticket(models.Model):
+    show = models.ForeignKey(Show, related_name="ticket", verbose_name="Сеанс", on_delete=models.PROTECT)
+    seat = models.ForeignKey(Seat, related_name="ticket", verbose_name="Место", on_delete=models.PROTECT)
+    sale = models.ForeignKey(Sale, related_name="ticket", verbose_name="Скидка", on_delete=models.PROTECT, blank=True, null=True)
+    go_back = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s, %s %s" % (self.show, self.seat, self.sale)
+
+
+def generate_code():
+    code = ""
+    for i in range(0, settings.BOOKING_CODE_LENGHT):
+        code += random.choice(string.digits)
+    return code
+
+class Booking(models.Model):
+
+    STATUS_CREATED = 'Создано'
+    STATUS_REDEEMED = 'Выкуплено'
+    STATUS_CANCELLATION = 'Отмена'
+
+    STATUS_CHOICES = (
+        (STATUS_CREATED, 'Создано'),
+        (STATUS_REDEEMED, 'Выкуплено'),
+        (STATUS_CANCELLATION, 'Отмена')
+    )
+
+    code = models.CharField(max_length=12, unique_for_date='created_at', default=generate_code, editable=False)
+    show = models.ForeignKey(Show, related_name="booking", verbose_name="Сеанс", on_delete=models.PROTECT)
+    seat = models.ManyToManyField(Seat, related_name="booking", verbose_name="Место")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name="Статус")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+
+    def __str__(self):
+        return "%s,    %s в %s" % (self.show, self.status, self.created_at.strftime('%d.%m.%Y %H:%M'))
