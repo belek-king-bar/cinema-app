51fa8fa4f9612753cf8abf4a8757b3639fcdf768
belek <belektolubaev@gmail.com>
2019-03-11 19:01:40 +0600

    Подключение базового url для axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import axios from 'axios';
+import {BASE_URL} from "./api-urls";
+
+axios.defaults.baseURL = BASE_URL;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
