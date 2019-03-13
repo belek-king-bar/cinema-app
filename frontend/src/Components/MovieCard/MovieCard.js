b6f3fe844fe9a3e621a29f42ef9abe693afa9f3d
belek <belektolubaev@gmail.com>
2019-03-13 23:45:39 +0600

    Изменение в MovieCard

--- frontend/src/Components/MovieCard/MovieCard.js
+++ frontend/src/Components/MovieCard/MovieCard.js
@@ -5,7 +5,7 @@ import Card from "../UI/Card/Card";
 // Компонент, который рисует карточку для фильма: постер, название и ссылку,
 // используя компонент UI/Card (карточка), основанный на стилях bootstrap.
 const MovieCard = props => {
-    const {movie, className} = props;
+    const {movie, onDelete} = props;
 
     // достаём данные из movie
     const {name, poster, id} = movie;
@@ -17,7 +17,7 @@ const MovieCard = props => {
     };
 
     // возвращаем (рисуем) карточку с данными из movie и ссылкой.
-    return <Card header={name} image={poster} link={link} className='h-100'/>;
+    return <Card header={name} image={poster} link={link} onDelete={onDelete} className='h-100'/>;
 };
 
 
