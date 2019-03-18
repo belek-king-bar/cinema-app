4f16666f701d89355b81d68ed51ed7089b0c54dc
belek <belektolubaev@gmail.com>
2019-03-18 15:48:36 +0600

    вывод сеансов для фильмов

--- frontend/src/Containers/MovieDetail/MovieDetail.js
+++ frontend/src/Containers/MovieDetail/MovieDetail.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
-import {MOVIES_URL} from "../../api-urls";
+import {MOVIES_URL, SHOWS_URL} from "../../api-urls";
 import {NavLink} from "react-router-dom";
 import MovieCategories from "../../Components/MovieCategories/MovieCategories";
 import axios from 'axios';
-
+import moment from 'moment';
+import ShowSchedule from "../../Components/ShowSchedule/ShowSchedule";
 
 // компонент, который выводит одну карточку с фильмом
 // фильм также загружается при выводе компонента на экран (mount),
@@ -11,7 +12,8 @@ import axios from 'axios';
 // и при переключении страниц исчезает с экрана, а потом снова маунтится.
 class MovieDetail extends Component {
     state = {
-        movie: null
+        movie: null,
+        shows: null
     };
 
     componentDidMount() {
@@ -22,9 +24,41 @@ class MovieDetail extends Component {
         // match.params.id - значение переменной, обозначенной :id в свойстве path Route-а.
         axios.get(MOVIES_URL + match.params.id)
             .then(response => {console.log(response.data); return response.data;})
-            .then(movie => this.setState({movie}))
+            .then(movie => {
+                this.setState({movie});
+
+                this.loadShows(movie.id);
+            })
             .catch(error => console.log(error));
     }
+
+    loadShows = (movieId) => {
+        // https://momentjs.com/ - библиотека для работы с датой и временем в JS
+        // более удобная, чем встроенный класс Date(). Не забудьте импортировать.
+        // установка: npm install --save moment (уже ставится вместе с реактом)
+        // импорт: import moment from 'moment';
+
+        // вернёт текущую дату со временем в формате ISO с учётом временной зоны
+        const startsAfter = moment().format('YYYY-MM-DD HH:mm');
+        // вернёт только дату на 3 дня вперёд от текущей в указанном формате
+        const startsBefore = moment().add(3, 'days').format('YYYY-MM-DD');
+
+        // encodeURI закодирует строку для передачи в запросе
+        // отличается от encodeURIComponent тем, что пропускает символы,
+        // входящие в формат URI, в т.ч. & и =.
+        const query = encodeURI(`movie_id=${movieId}&starts_after=${startsAfter}&starts_before=${startsBefore}`);
+        axios.get(`${SHOWS_URL}?${query}`).then(response => {
+            console.log(response);
+            this.setState(prevState => {
+                let newState = {...prevState};
+                newState.shows = response.data;
+                return newState;
+            })
+        }).catch(error => {
+            console.log(error);
+            console.log(error.response);
+        });
+    };
 
     render() {
         // если movie в state нет, ничего не рисуем.
@@ -54,6 +88,9 @@ class MovieDetail extends Component {
 
             {/* назад */}
             <NavLink to='' className="btn btn-primary">Movies</NavLink>
+
+            {this.state.shows ? <ShowSchedule shows={this.state.shows} c_type={'hall'}/> : null}
+
         </div>;
     }
 }
