c5515586e9eb9dad555eba0e27fef2982ccf8877
belek <belektolubaev@gmail.com>
2019-03-18 15:48:53 +0600

    Изменение

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,28 +10,28 @@ import HallList from "./Containers/HallList/HallList";
 import HallDetail from "./Containers/HallDetail/HallDetail"
 import HallAdd from "./Containers/HallAdd/HallAdd"
 import HallEdit from "./Containers/HallEdit/HallEdit"
-import Layout from "./Layout"
+import Layout from "./Components/Layout/Layout"
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
                 <BrowserRouter>
                     <Layout>
-                        <Switch>
-                            <Route path="/halls/add" component={HallAdd}/>
-                            <Route path="/halls/:id/edit" component={HallEdit}/>
-                            <Route path="/halls/:id" component={HallDetail}/>
-                            <Route path="/halls" component={HallList}/>
-                            <Route path="/movies/add" component={MovieAdd}/>
-                            <Route path="/movies/:id/edit" component={MovieEdit}/>
-                            {/* :id обозначает переменную id */}
-                            <Route path="/movies/:id" component={MovieDetail}/>
-                            <Route path="/" component={MovieList}/>
-                    </Switch>
+                        <div className="container">
+                            <Switch>
+                                <Route path="/halls/add" component={HallAdd}/>
+                                <Route path="/halls/:id/edit" component={HallEdit}/>
+                                <Route path="/halls/:id" component={HallDetail}/>
+                                <Route path="/halls" component={HallList}/>
+                                <Route path="/movies/add" component={MovieAdd}/>
+                                <Route path="/movies/:id/edit" component={MovieEdit}/>
+                                {/* :id обозначает переменную id */}
+                                <Route path="/movies/:id" component={MovieDetail}/>
+                                <Route path="/" component={MovieList}/>
+                            </Switch>
+                        </div>
                     </Layout>
                 </BrowserRouter>
-            </div>
         );
     }
 }
--- frontend/src/api-urls.js
+++ frontend/src/api-urls.js
@@ -2,5 +2,6 @@ const BASE_URL = 'http://localhost:8000/api/v1';
 const MOVIES_URL = '/movies/';
 const HALLS_URL = '/halls/';
 const CATEGORIES_URL = '/categories/';
+const SHOWS_URL = '/shows/';
 
-export {MOVIES_URL, CATEGORIES_URL, BASE_URL, HALLS_URL}
+export {MOVIES_URL, CATEGORIES_URL, BASE_URL, HALLS_URL, SHOWS_URL}
