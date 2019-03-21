67a668d9de8cbf5bc87e4355ca13280c2b75311a
belek <belektolubaev@gmail.com>
2019-03-21 20:51:07 +0600

    создание collapse

--- frontend/src/Components/Navbar/Menu/Menu.js
+++ frontend/src/Components/Navbar/Menu/Menu.js
@@ -6,13 +6,18 @@ class Menu extends Component {
         collapse: true
     };
 
+    toggle = () => {
+        this.setState({collapse: !this.state.collapse});
+    };
+
     render() {
         return <Fragment>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            <button  onClick={this.toggle} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className={(this.state.collapse ? "collapse" : "") + " navbar-collapse"}
+                 id="navbarNav">
                 <ul className="navbar-nav">
                     <MenuItem to="/">Фильмы</MenuItem>
                     <MenuItem to="/movies/add">Добавить фильм</MenuItem>
