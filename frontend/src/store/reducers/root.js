42de6e257ede39fd3a15b81f5902f67c8ce19b4a
belek <belektolubaev@gmail.com>
2019-04-04 01:57:37 +0600

    Главный root

--- frontend/src/store/reducers/root.js
+++ frontend/src/store/reducers/root.js
@@ -3,14 +3,30 @@ import loginReducer from "./login";
 import authReducer from "./auth";
 import tokenLoginReducer from "./app";
 import movieListReducer from "./movie_list";
+import movieDetailReducer from "./movie_detail"
 import movieEditReducer from "./movie_edit"
+import movieAddReducer from "./movie_add"
+import hallListReducer from "./hall_list"
+import hallDetailReducer from "./hall_detail"
+import hallEditReducer from "./hall_edit"
+import hallAddReducer from "./hall_add"
+import registerReducer from "./register"
+import userDetailReducer from "./user_detail"
 
 const rootReducer = combineReducers({
     login: loginReducer,
+    register: registerReducer,
     auth: authReducer,
     app: tokenLoginReducer,
     movieList: movieListReducer,
-    movieEdit: movieEditReducer
+    movieDetail: movieDetailReducer,
+    movieEdit: movieEditReducer,
+    movieAdd: movieAddReducer,
+    hallList: hallListReducer,
+    hallDetail: hallDetailReducer,
+    hallEdit: hallEditReducer,
+    hallAdd: hallAddReducer,
+    user: userDetailReducer
 });
 
 export default rootReducer;
